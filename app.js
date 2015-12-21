8d6bb70193357caa13edc9f7eab6a02946a60f9d
RoyTimes <roytimes@icloud.com>
2015-12-22 06:05:06 +0800

    finish up the project

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var routes = require('./router');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
