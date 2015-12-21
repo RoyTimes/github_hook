e72c19f1d18b9b4d96d625b001aaf688cb31f42b
RoyTimes <roytimes@icloud.com>
2015-12-22 04:47:07 +0800

    fix bugs

--- router.js
+++ router.js
@@ -1,19 +1,24 @@
 var express = require ('express');
 var _ = require ('underscore');
 // var repos = require ('./repos');
-var run_cmd = require ('./run_cmd');
+var run_cmd = require ('./run_cmd').run_cmd;
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.post('/deploy', function(req, res) {
+router.get('/deploy', function(req, res) {
 
     var payload;
-    if (typeof req.body.payload === 'object')
-        payload = req.body.payload;
-    else payload = JSON.parse(req.body.payload);
+    if (typeof req.body === 'object')
+        {payload = req.body.payload;}
+
+
+    else {
+        console.log(req.body);
+        payload = JSON.parse(req.body);
+    }
 
     run_cmd("cd ~/skye.kiwi && git pull");
 
