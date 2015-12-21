c7a577dc06f8a6ae9c8d044cf8b800249e462da5
RoyTimes <roytimes@icloud.com>
2015-12-22 06:16:59 +0800

    Finish Up

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# github_hook
-Node.js project for auto-deploy using github hook
+#### Hi, guys. I am <strong>Song Zhou</strong>.
+
+I created this small project.<br>
+
+- I got boared one night and coded (just for fun) a Node.js program for auto-deployment using webhook provided by Github.<br>
+- If this project helps you a little, I would be really pleased if you <b>Star</b> this project and <b>Follow</b> me.<br>
+- It's really light and super easy to use. Hope you guys enjoy it. : )
+
+<br>
+### How to run this project?
+1. Clone the repo [@Github](http://github.com/RoyTimes/github_hook)<br>
+2. Install the NPM dependencies by `npm install`<br>
+3. Write your repo settings into `repos.json`<br>
+4. Finally, just `nohup npm start` the project!<br>
+
+DONE!
+
+<hr>
+There would be a beautiful static web page runs on `localhost` and when there is a `POST` request onto `localhost/deploy` the program would run the cmd written in the JSON file : )  That's all for it!
+
+
+@ MIT LICENSED
--- router.js
+++ router.js
@@ -1,33 +1,25 @@
 var express = require ('express');
 var _ = require ('underscore');
-// var repos = require ('./repos');
+var repos = require ('./repos');
 var run_cmd = require ('./run_cmd').run_cmd;
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/deploy', function(req, res) {
-
+router.post('/deploy', function(req, res) {
     var payload;
     if (typeof req.body === 'object')
         {payload = req.body.payload;}
-
-
     else {
         console.log(req.body);
         payload = JSON.parse(req.body);
     }
-
-    run_cmd("cd ~/skye.kiwi && git pull");
-    res.send('deployed');
-
-    // _.each (repos, function (repo) {
-    //     if (repo.name === payload.repository.name &&
-    //         payload.ref.indexOf(repo.ref) >= 0) {
-    //             repo.action(repo.name, repo.ref, payload);
-    //     } res.send('deployed');
-    // });
+    _.each (repos, function (repo) {
+        if (repo.name === payload.repository.name &&
+            payload.ref.indexOf(repo.ref) >= 0) {
+                run_cmd(repo.deploy);
+        } res.send('deployed');
+    });
 });
 module.exports = router;
